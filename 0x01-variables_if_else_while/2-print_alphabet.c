4c11c658de61978f44596cf48c91c6da2a0e666d
link225 <bazaroov@gmail.com>
2022-03-11 09:27:11 -0800

    task 2.00

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,21 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/* more headers goes there */
+
+/**
+ *  main - the main app function
+ * Return: 0 for success
+ */
+int main(void)
+{
+	char a;
+
+	for (a = 'a'; a <= 'z'; ++a)
+		putchar(a);
+		
+	putchar('\n');
+
+
+	return (0);
+}
