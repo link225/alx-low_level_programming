c5f8c06e7e9d0dd377d205221c28011379dba1a9
link225 <bazaroov@gmail.com>
2022-03-11 10:37:12 -0800

    task 6.00

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,22 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/* more headers goes there */
+
+/**
+ *  main - the main app function
+ * Return: 0 for success
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 0; i <= 9; i++)
+	{
+		putchar(i+'0');
+	}
+	putchar('\n');
+
+
+	return (0);
+}
