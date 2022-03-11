cd51811201a837ef25feaff5034901e4b157cc9e
link225 <bazaroov@gmail.com>
2022-03-11 10:07:13 -0800

    task 4.00

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,25 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/* more headers goes there */
+
+/**
+ *  main - the main app function
+ * Return: 0 for success
+ */
+int main(void)
+{
+	char a;
+
+	for (a = 'a'; a <= 'z'; ++a)
+	{
+		if (a != 'e' && a != 'q')
+		{
+			putchar(a);
+		}
+	}
+	putchar('\n');
+
+
+	return (0);
+}
