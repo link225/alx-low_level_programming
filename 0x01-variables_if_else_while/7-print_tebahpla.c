0e73b2f10caa6b0d9118ce7973cb876d3c4b2cc8
link225 <bazaroov@gmail.com>
2022-03-14 10:08:31 -0700

    task 7.01

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,22 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/* more headers goes there */
+
+/**
+ *  main - the main app function
+ * Return: 0 for success
+ */
+int main(void)
+{
+	char z = 'z';
+	while(z >= 'a')
+	{
+		putchar(z);
+		z--;
+	}
+	putchar('\n');
+
+
+	return (0);
+}
