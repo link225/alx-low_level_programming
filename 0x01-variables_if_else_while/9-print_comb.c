4c844bda6f58de5ea5a584db70effcac82b94d73
link225 <bazaroov@gmail.com>
2022-03-14 10:36:41 -0700

    task 9.00

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,28 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/* more headers goes there */
+
+/**
+ *  main - the main app function
+ * Return: 0 for success
+ */
+int main(void)
+{
+	int i = 0;
+
+	while (i < 10)
+	{
+		putchar(i + '0');
+		if (i != 9)
+		{
+			putchar(' ');
+			putchar(',');
+		}
+		i++;
+	}
+	putchar('\n');
+
+
+	return (0);
+}
