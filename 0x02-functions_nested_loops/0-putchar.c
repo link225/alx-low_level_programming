9c280c037656d21760d898635925441d30cfacb2
link225 <bazaroov@gmail.com>
2022-03-15 12:43:23 -0700

    task 0.00

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,17 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+
+/* more headers goes there */
+
+/**
+ *  main - the main app function
+ * Return: 0 for success
+ */
+int main(void)
+{
+	printf("_putchar\n");
+
+	return (0);
+}
