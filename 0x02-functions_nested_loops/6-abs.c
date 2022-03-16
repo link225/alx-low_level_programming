f06d153ea9ad5f85ba6b55214722a1df466e87eb
link225 <bazaroov@gmail.com>
2022-03-15 23:33:47 -0700

    task 6.00

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,13 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ *_abs- the test of entry number
+ * @c : is int
+ * Return: int absolute;
+ **/
+int _abs(int c)
+{
+	return (abs(c));
+}
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -12,7 +12,9 @@ int _islower(int c);
 
 int _isalpha(int c);
 
-int print_sign(int c);
+int print_sign(int n);
+
+int _abs(int);
 
 #endif
 
