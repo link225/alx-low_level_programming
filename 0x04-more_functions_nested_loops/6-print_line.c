3ef43ae406893b2aaece7a9791680b3b6084f96b
link225 <bazaroov@gmail.com>
2022-03-17 23:32:44 -0700

    task 6.11

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -12,10 +12,13 @@ void print_line(int n)
 
 	if (n > 0)
 	{
-		for (i = 0; i <= n; i++)
+		for (i = 0; i < n; i++)
 		{
 			_putchar(95);
 		}
+		_putchar('\n');
+	} else
+	{
+		_putchar('\n');
 	}
-	_putchar('\n');
 }
