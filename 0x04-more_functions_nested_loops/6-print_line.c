9f3074833752910566f079c6fb7a2c58f8f11080
link225 <bazaroov@gmail.com>
2022-03-17 22:31:07 -0700

    task 6.01

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -2,24 +2,24 @@
 
 /**
  * print_line - print numbers of line in entry
- * @c: int for loop
+ * @n: int for loop
  * Return: void
  */
 
-void print_line(int c)
+void print_line(int n)
 {
 	int i;
 
-	if (c > 0)
+	if (c <= 0)
 	{
-		for (i = 0; i <= c; i++)
+		_putchar('\n');
+	} else
+	{
+		for (i = 0; i <= n; i++)
 		{
 			_putchar('_');
 		}
 		_putchar('\n');
-	} else
-	{
 
-		_putchar('\n');
 	}
 }
