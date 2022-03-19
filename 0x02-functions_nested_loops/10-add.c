321274c0986ba861e77819eacd0e1e16f6c28aaa
link225 <bazaroov@gmail.com>
2022-03-19 10:16:47 -0700

    task 10.00

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * add - make sum of two number 
+ * @c : is int
+ * @a: int number two
+ * Return: int is the last digit of @c;
+ **/
+int add(int c, int a)
+{
+
+	return (c + a);
+}
