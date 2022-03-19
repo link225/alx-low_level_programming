7460c1afc15d49cc7aae33c5dec4241abcdb3b38
link225 <bazaroov@gmail.com>
2022-03-19 11:04:13 -0700

    task 11.01

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -3,10 +3,10 @@
 
 /**
  * print_to_98 - print from n to 98
- * @c : is int
+ * @n : is int
  * Return: int is the last digit of @c;
  **/
-void print_to_98(int c)
+void print_to_98(int n)
 {
 	if (n < 98)
 	{
@@ -21,7 +21,7 @@ void print_to_98(int c)
 	{
 		for (n = n; n > 98; n--)
 		{
-			printf("%d, ");
+			printf("%d, ", n);
 		}
 		printf("%d\n", 98);
 	}
