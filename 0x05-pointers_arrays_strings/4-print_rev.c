b5150aee3ec45aa9f84a9d74c4500ca667d71a69
link225 <bazaroov@gmail.com>
2022-03-21 14:18:11 -0700

    task 4.07

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -8,18 +8,15 @@
 
 void print_rev(char *s)
 {
-	int i, j;
-	
-	j = 0;
+	int i;
 
 	for (i = 0; s[i] != '\0'; ++i)
 	{
 	}
-
-	while (s[j] != '\0')
+	while (i >= 0)
 	{
 		_putchar(*(s + i));
 		i--;
 	}
-	_putchar('\n');
+	/*_putchar('\n');*/
 }
