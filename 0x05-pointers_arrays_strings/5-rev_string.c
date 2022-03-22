155d224a124efe392117a821ead37b77f7de8e8d
link225 <bazaroov@gmail.com>
2022-03-21 19:13:57 -0700

    task 5.00

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * rev_string - pass to newline
+ * @s: char is pointer
+ * Return: void
+ */
+
+void rev_string(char *s)
+{
+	char ch;
+	int i, c, l;
+
+	
+	for (i = 0; s[i] != '\0'; i++);
+
+	l = i;
+
+	for (i--, c = 0; c < l / 2; c++, i--)
+	{
+		ch = s[c];
+		s[c] = s[i];
+		s[i] = ch;
+	}
+}
