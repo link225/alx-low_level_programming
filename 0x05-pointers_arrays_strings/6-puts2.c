580abd29bf808a742b27c9305d7a6f52d2498701
link225 <bazaroov@gmail.com>
2022-03-21 19:33:45 -0700

    task 6.00

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * rev_string - pass to newline
+ * @s: char is pointer
+ * Return: void
+ */
+
+void puts2(char *s)
+{
+	int i;
+
+	for (i = 0; s[i] != '\0' ; i++)
+	{
+		if(i % 2 == 0)
+		{
+			_putchar(s[i]);
+		}
+	}
+	_putchar('\n');
+}
