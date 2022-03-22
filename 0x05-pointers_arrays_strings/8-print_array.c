250a356b1bcb20609651555009bdcbf5778c6413
link225 <bazaroov@gmail.com>
2022-03-21 20:20:00 -0700

    task 6.05

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -13,14 +13,14 @@ void print_array(int *a, int n)
 
 	for (i = 0; i < n; i++)
 	{
-		if (i != n)
+		if (i == (n - 1))
 		{
 			printf("%d", a[i]);
-			printf(", ");
 		}
 		else
 		{
 			printf("%d", a[i]);
+			printf(", ");
 		}
 	}
 	printf("\n");
