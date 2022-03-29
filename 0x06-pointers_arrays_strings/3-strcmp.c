231560b4403394ac77ee94f74cb6fe7ebbb3ee48
link225 <bazaroov@gmail.com>
2022-03-29 06:29:11 -0700

    task 3.00

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,13 @@
+#include "main.h"
+#include <string.h>
+/**
+ * *_strcmp - concacte two string
+ * @s1: first char is pointer
+ * @s2: second char is pointer
+ * Return: int 
+ */
+
+int *_strcmp(char *s1, char *s2)
+{
+	return (strcmp(s1, s2));
+}
