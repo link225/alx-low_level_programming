3cef16f101d870e2a7fe51c0a3e11e68f78dbdfb
link225 <bazaroov@gmail.com>
2022-03-29 08:54:14 -0700

    task 101.00

---
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -0,0 +1,26 @@
+#include "main.h"
+#include <string.h>
+/**
+ * print_number - remplace letter
+ * @a: first char is pointer
+ * Return: void 
+ */
+void print_number(int a)
+{
+	unsigned int n1;
+
+	n1 = n;
+
+	if (n < 0)
+	{
+		_putchar('-');
+		n1 = -n;
+	}
+
+	if (n1 / 10 != 0)
+	{
+		print_number(n1 / 10);
+	}
+	_putchar((n1 % 10) + '0');
+
+}
