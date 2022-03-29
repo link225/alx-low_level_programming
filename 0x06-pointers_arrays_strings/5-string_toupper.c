26a253dec93fe3fbb5312cd92dded618013f8e88
link225 <bazaroov@gmail.com>
2022-03-29 07:19:41 -0700

    task 5.00

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include <string.h>
+/**
+ * string_toupper - concacte two string
+ * @a: first char is pointer
+ * Return: char
+ */
+char *string_toupper(char *a)
+{
+	int i;
+
+	for (i = 0; a[i] != '\0'; i++)
+	{
+		if (a[i] > 98 && a[i] < 123)
+		{
+			a[i] -= 32;
+		}
+	}
+	return (a);
+}
