ea39f5e5bdea113bf142cd3575433d4471e83adb
link225 <bazaroov@gmail.com>
2022-04-10 07:19:30 -0700

    task 0.00

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ * *_strcat - concacte two string
+ * @s: first char is pointer
+ * @b: second char is pointer
+ * @n: unsigned int
+ * Return: char
+ */
+
+char *_memset(char *s, char b, unsigned int n)
+{
+	unsigned int i;
+
+
+	return (memset(s,b,n);
+}
