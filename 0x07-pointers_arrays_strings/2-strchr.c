b88ab339665fa372eba237e45714fc0f092ac50f
link225 <bazaroov@gmail.com>
2022-04-10 07:46:45 -0700

    task 2.00

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ * *_strchr - concacte two string
+ * @s: first char is pointer
+ * @n: second char is pointer
+ * Return: char
+ */
+
+char *_strchr(char *s, char n)
+{
+
+	return (strchr(s, n));
+}
