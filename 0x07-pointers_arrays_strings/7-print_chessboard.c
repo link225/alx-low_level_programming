9161adcc12af61675c37553fc63f35e15dd19bde
link225 <bazaroov@gmail.com>
2022-04-10 10:54:50 -0700

    task 7.00

---
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -0,0 +1,25 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ * print_chessboard - concacte two string
+ * @a: first char is pointer
+ *
+ * Description: print_chessboard
+ * Return: int
+ */
+
+void print_chessboard(char (*a)[8])
+{
+	int i, j;
+
+	for (i = 0; i < 8; i++)
+	{
+		for (j = 0; j < 8; j++)
+		{
+			_putchar(a[i][j]);
+		}
+		_putchar('\n');
+	}
+
+}
